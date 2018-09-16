9fc119a1173db2f1a47dac1c972ede2b24a07ab9
Mihir Chaturvedi <plibither8@gmail.com>
2018-09-16 19:29:08 +0530

    app: Add routes and middleware

--- app.js
+++ app.js
@@ -2,16 +2,22 @@ const express = require('express');
 
 const app = express();
 
-// //Setting public directory
-// app.use(express.static(__dirname + '/public'));
-
-// //Setting view engine
-// app.set('view engine', 'pug');
-// app.set('views', __dirname + '/views/pages');
-
-app.set('json spaces', 2);
-
+// Main routes handler
 const home = require('./routes/index');
 app.use('/', home);
+
+// API routes handler
+const apiRoute = require('./routes/api');
+app.use('/api', apiRoute);
+
+//Setting public directory
+app.use(express.static(__dirname + '/public'));
+
+//Setting view engine
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views/pages');
+
+// Prettify JSON files when sending through res.json()
+app.set('json spaces', 2);
 
 app.listen(process.env.PORT || 5000);

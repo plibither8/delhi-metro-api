b7d6147e9931b64f0136c3362e37d59e08476186
Mihir Chaturvedi <plibither8@gmail.com>
2018-09-18 12:47:21 +0530

    Make routes code less verbose by using optional parameters

--- routes/api.js
+++ routes/api.js
@@ -6,35 +6,12 @@ router.get('/', (req, res) => {
     return res.json(api.getMetaInfo()); 
 });
 
-router.get('/lines', (req, res) => {
-    return res.json(api.getLines());
-});
-
-router.get('/lines/:line', (req, res) => {
-    const { line } = req.params;
-    return res.json(api.getLines(line));
-});
-
-router.get('/lines/:line/:id', (req, res) => {
-    const { line, id } = req.params;
-    return res.json(api.getLines(line, id));
-});
-
-router.get('/lines/:line/:id/:key', (req, res) => {
+router.get('/lines/:line?/:id?/:key?', (req, res) => {
     const { line, id, key } = req.params;
     return res.json(api.getLines(line, id, key));
 });
 
-router.get('/stations', (req, res) => {
-    return res.json(api.getStations());
-});
-
-router.get('/stations/:station', (req, res) => {
-    const { station } = req.params;
-    return res.json(api.getStations(station));
-});
-
-router.get('/stations/:station/:key', (req, res) => {
+router.get('/stations/:station?/:key?', (req, res) => {
     const { station, key } = req.params;
     return res.json(api.getStations(station, key));
 });
@@ -47,11 +24,7 @@ router.get('/list/lines', (req, res) => {
     return res.json(api.getLineList());
 });
 
-router.get('/list/stations', (req, res) => {
-    return res.json(api.getStationList());
-});
-
-router.get('/list/stations/:line', (req, res) => {
+router.get('/list/stations/:line?', (req, res) => {
     const { line } = req.params;
     return res.json(api.getStationList(line));
 });
